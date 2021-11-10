b29f181a4393219445612a1941c1f23ad1283c39
magichat <magichat@localhost>
2021-11-10 15:25:31 -0500

    holodeck helper small smurfs

--- holodeck.py
+++ holodeck.py
@@ -44,46 +44,49 @@ t4dtz.data.body = now.strftime('%m%d%y @%H:%M ') + timeZDifferenceStr
 ##"""
 
 #this will only work with the CYCLES rendering engine selected - if cycles
+#output settings
+useTheseSettings = False
+if (useTheseSettings):
+    #RENDER SAMPLE COUNT
+    bpy.context.scene.cycles.samples = 128
 
-#RENDER SAMPLE COUNT
-bpy.context.scene.cycles.samples = 48
+    #LIGHT PATHS (MAX BOUNCES)
+    bpy.context.scene.cycles.max_bounces = 32
+
+    #LIGHT PATHS (diffuse, glossy, transparency, transmission, volume)
+    bpy.context.scene.cycles.diffuse_bounces = 30
+    bpy.context.scene.cycles.glossy_bounces = 30
+    bpy.context.scene.cycles.transparent_max_bounces = 31
+    bpy.context.scene.cycles.transmission_bounces = 32
+    bpy.context.scene.cycles.volume_bounces = 32
+
+    #OTHER RENDER SETTINGS FOR DYNAMIC DISPLAY
+    #ambient occulusion
+    bpy.context.scene.world.light_settings.use_ambient_occlusion = False
+    #render tiles
+    bpy.context.scene.render.tile_x = 48
+    bpy.context.scene.render.tile_y = 48    #caustics_reflective & caustics_refractive
+    bpy.context.scene.cycles.caustics_reflective = True
+    bpy.context.scene.cycles.caustics_refractive = True
+    #sampling > adaptive sampling
+    bpy.context.scene.cycles.use_adaptive_sampling = False
+
+
+#######################################################
+#######################################################
 cyclesRenderSamples=bpy.data.objects['CYCLES_RENDER_SAMPLES']
 cyclesRenderSamples.data.body=str(bpy.context.scene.cycles.samples)
-
-
-#LIGHT PATHS (MAX BOUNCES)
-bpy.context.scene.cycles.max_bounces = 12
 maxBounces=bpy.data.objects['MAX_BOUNCES']
 maxBounces.data.body=str(bpy.context.scene.cycles.max_bounces)
-
-#LIGHT PATHS (diffuse, glossy, transparency, transmission, volume)
-bpy.context.scene.cycles.diffuse_bounces = 12
-bpy.context.scene.cycles.glossy_bounces = 12
-bpy.context.scene.cycles.transparent_max_bounces = 12
-bpy.context.scene.cycles.transmission_bounces = 12
-bpy.context.scene.cycles.volume_bounces = 12
-
 lightPathsCollective=bpy.data.objects['LIGHT_PATHS_COLLECTIVE']
 lightPathsCollective.data.body="      ++      diffuse_bounces:    "+str(bpy.context.scene.cycles.diffuse_bounces)+"\n      ++      glossy_bounces:    "+str(bpy.context.scene.cycles.glossy_bounces)+"\n      ++      transparent_max_bounces:    "+str(bpy.context.scene.cycles.transparent_max_bounces)+"\n      ++      transmission_bounces:    "+str(bpy.context.scene.cycles.transmission_bounces)+"\n      ++      volume_bounces:    "+str(bpy.context.scene.cycles.volume_bounces)
-
-#OTHER RENDER SETTINGS FOR DYNAMIC DISPLAY
 otherRenderInfo=bpy.data.objects['OTHER_RENDER_INFO']
-#ambient occulusion
-bpy.context.scene.world.light_settings.use_ambient_occlusion = False
 otherRenderInfo.data.body="ambient_occlusion: "+str(bpy.context.scene.world.light_settings.use_ambient_occlusion)+"\n"
-#render tiles
-bpy.context.scene.render.tile_x = 96
-bpy.context.scene.render.tile_y = 96
 otherRenderInfo.data.body+="render.tile_x: "+str(bpy.context.scene.render.tile_x)+"  render.tile_y: "+str(bpy.context.scene.render.tile_y)+"\n"
-#caustics_reflective & caustics_refractive
-bpy.context.scene.cycles.caustics_reflective = False
-bpy.context.scene.cycles.caustics_refractive = True
+otherRenderInfo.data.body+="render.tile_x: "+str(bpy.context.scene.render.tile_x)+"  render.tile_y: "+str(bpy.context.scene.render.tile_y)+"\n"
 otherRenderInfo.data.body+="caustics_reflective: "+str(bpy.context.scene.cycles.caustics_reflective)+"\n"
 otherRenderInfo.data.body+="caustics_refractive: "+str(bpy.context.scene.cycles.caustics_refractive)+"\n"
-#sampling > adaptive sampling
-bpy.context.scene.cycles.use_adaptive_sampling = False
 otherRenderInfo.data.body+="adaptive_sampling: "+str(bpy.context.scene.cycles.use_adaptive_sampling)+"\n"
-
 #######################################################
 #######################################################
 
@@ -103,11 +106,23 @@ if (isAnim):
 else:
     bpy.context.scene.render.resolution_x = 1920
     bpy.context.scene.render.resolution_y = 1080
+    #bpy.context.scene.render.resolution_x = 3840
+    #bpy.context.scene.render.resolution_y = 2160
 
     #bpy.context.scene.render.filepath = "Desktop/outs-lost-found/holodeck_" + now.strftime('%m%d%y-%H%M')+".jpg"
-    bpy.context.scene.render.filepath = "//holodeck-outs/holodeck_" + now.strftime('%m%d%y-%H%M')+".jpg"
-    bpy.context.scene.render.image_settings.file_format = 'JPEG'
-    bpy.context.scene.render.image_settings.quality = 80
-
+    ###JPG
+    ###bpy.context.scene.render.filepath = "//holodeck-outs/holodeck_" + now.strftime('%m%d%y-%H%M')+".jpg"
+    ###bpy.context.scene.render.image_settings.file_format = 'JPEG'
+    ###bpy.context.scene.render.image_settings.quality = 86
+    ##PNG
+    bpy.context.scene.render.image_settings.file_format = 'PNG'
+    bpy.context.scene.render.filepath = "//holodeck-outs/holodeck_" + now.strftime('%m%d%y-%H%M')+".png"
+    
     ##Render the default render (same as F12 only better)
     bpy.ops.render.render('INVOKE_DEFAULT', animation=False, write_still=True)
+
+#stairs 3d model
+#seats 3d model
+#half circle of stairs and seats 3d model
+
+##py for generating extrusion to holo core (from bottom view of 3d object)

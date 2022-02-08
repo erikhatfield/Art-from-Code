1dc5722d26867e700fbbb236bd75fc77567b8ad7
EL Hat <2522870+erikhatfield@users.noreply.github.com>
2022-02-07 20:22:18 -0500

    included shell script for running python from terminal in osx

---
+++ SH/art-from-code.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#blender
+open ~/Dev/BL/art-from-code
+
+cd ~/Dev/BL/art-from-code
+
+git pull https://github.com/erikhatfield/Art-from-Code.git
+
+rm -rf ~/Dev/BL/art-from-code/BL/temp_auto_grid.blend 
+cp -a ~/Dev/BL/art-from-code/BL/grid.blend ~/Dev/BL/art-from-code/BL/temp_auto_grid.blend
+
+#blender -b ~/Dev/BL/art-from-code/BL/temp_auto_grid.blend -P ~/Dev/BL/art-from-code/grid.py
+# echo "alias blender=/Applications/Blender.app/Contents/MacOS/blender" >> ~/.bash_profile
+#source ~/.bash_profile
+/Applications/Blender.app/Contents/MacOS/blender ~/Dev/BL/art-from-code/BL/temp_auto_grid.blend -P ~/Dev/BL/art-from-code/grid.py
+
+
+###############################################################################
+#                                                                             #
+###############################################################################
+
+#open ~/Dev/BL/art-from-code/output

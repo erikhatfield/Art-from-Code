0ef79095d04488fae8710bcbb8b998dd73bec604
magichat <magichat@localhost>
2022-03-04 12:42:38 -0500

    expanded os.walk arrays for understanding purposes

--- directory-visualizer.py
+++ directory-visualizer.py
@@ -1,18 +1,44 @@
 # Python program to recursively traverse directories starting at a path
 # and provide data visualization for all the sub-directories and files 
-    
+
 import bpy
 import os 
-    
-# List to store all directories 
-ListOfDir = []
-    
+
+# Lists to store all directories, paths, and roots
+ListOfAll = []
+ListOfDirs = []
+ListOfFiles = []
+ListOfRoots = []
+
 # Traversing through '/tmp/' the default output path in blender 3 pt oh one
 for root, dirs, files in os.walk('/tmp/'): 
-    
+
     # Adding the empty directory to list 
-    ListOfDir.append((root, dirs, files)) 
+    ListOfAll.append((root, dirs, files)) 
+    ListOfDirs.append((dirs)) 
+    ListOfFiles.append((files)) 
+    ListOfRoots.append((root)) 
   
 print("List of all sub-directories and files:") 
-for i in ListOfDir:
+for i in ListOfAll:
     print(i)
+
+print("\n\n")
+
+print("List of all directories:") 
+for i in ListOfDirs:
+    print(i)
+
+print("\n\n")
+
+print("List of all files:") 
+for i in ListOfFiles:
+    print(i)
+
+print("\n\n")
+
+print("List of all roots:") 
+for i in ListOfRoots:
+    print(i)
+
+
